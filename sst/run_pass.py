73a342d75a228d9bdf060a5aeafb090902ecee05
karol-lub-stack <karol-lub-stack@users.noreply.github.com>
2026-06-12 16:16:50 +0200

    Add run_pass.py entry points for wvd/ and sst/

---
+++ sst/run_pass.py
@@ -0,0 +1,2 @@
+# Entry point for running the SST estimator on a single pass.
+# To be implemented.
---
+++ wvd/run_pass.py
@@ -0,0 +1,2 @@
+# Entry point for running the SPWVD estimator on a single pass.
+# To be implemented.
